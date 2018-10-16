c0929420379d468fac6333f203aba88013196567
Bruno Pimentel <bpimentel@mastertech.tech>
2018-10-16 20:23:53 -0300

    Resolver ex2 e 3

--- scripts.js
+++ scripts.js
@@ -1,16 +1,45 @@
 const exibirAventura = () => {
+    if($('#nav-aventura').hasClass('ativa')){
+        exibirTodas();
+        return;
+    }
+
+    $('nav span').removeClass('ativa');
+    $('#nav-aventura').addClass('ativa');
+    
     $('div').hide();
     $('.aventura').show();
 }
 
 const exibirComedia = () => {
+    if($('#nav-comedia').hasClass('ativa')){
+        exibirTodas();
+        return;
+    }
+
+    $('nav span').removeClass('ativa');
+    $('#nav-comedia').addClass('ativa');
+    
     $('div').hide();
     $('.comedia').show();
 }
 
 const exibirDrama = () => {
+    if($('#nav-drama').hasClass('ativa')){
+        exibirTodas();
+        return;
+    }
+
+    $('nav span').removeClass('ativa');
+    $('#nav-drama').addClass('ativa');
+    
     $('div').hide();
     $('.drama').show();
+}
+
+const exibirTodas = () => {
+    $('nav span').removeClass('ativa');
+    $('div').show();
 }
 
 $('#nav-aventura').click(exibirAventura);
